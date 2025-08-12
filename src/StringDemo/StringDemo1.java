12e704b8f3d08706430383d21766a8ad534272f1
Admin <amolujagare@gmail.com>
2025-08-12 22:42:10 +0530

    JAVA

---
+++ src/StringDemo/StringDemo1.java
@@ -0,0 +1,17 @@
+package StringDemo;
+
+public class StringDemo1 {
+
+    public static void main(String[] args) {
+        String str = "This is my program";
+        System.out.println(str.length());
+        System.out.println(str.charAt(14));
+
+        System.out.println(str.toUpperCase());
+
+        String myString = "SONGS ARE NICE";
+        System.out.println(myString.toLowerCase());
+
+
+    }
+}
---
+++ src/StringDemo/SubstringDemo.java
@@ -0,0 +1,12 @@
+package StringDemo;
+
+public class SubstringDemo {
+
+    public static void main(String[] args) {
+
+        String str = "This is my program";
+
+        System.out.println(str.substring(13));
+        System.out.println(str.substring(8,15));
+    }
+}
